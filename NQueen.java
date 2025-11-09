c5fc3b108796229036714b0e83685230d2a1b843
ChinmayiBhosale <chinmayibhosale28@gmail.com>
2025-11-09 21:48:08 +0530

    NQueen.java

--- NQueen.java
+++ NQueen.java
@@ -77,3 +77,76 @@ public class NQueen {
         solveNQ(n);
     }
 }
+
+
+
+
+public class nQueen {
+    static final int N = 8;
+
+    static void printSolution(int board[][]) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                System.out.print((board[i][j] == 1 ? "Q " : ". "));
+            System.out.println();
+        }
+    }
+
+    static boolean isSafe(int board[][], int row, int col) {
+        for (int i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        for (int i = row, j = col; j >= 0 && i < N; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
+    static boolean solveNQUtil(int board[][], int col) {
+        if (col >= N)
+            return true;
+
+        for (int i = 0; i < N; i++) {
+            if (isSafe(board, i, col)) {
+                board[i][col] = 1;
+                if (solveNQUtil(board, col + 1))
+                    return true;
+                board[i][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = new int[N][N];
+        board[0][0] = 1;
+
+        if (!solveNQUtil(board, 1))
+            System.out.println("Solution does not exist");
+        else
+            printSolution(board);
+    }
+}
+
+Time Complexity: O(N!)
+Space Complexity: O(N)
+
+
+place q1 at 1,1
+place q2 at 2,3
+place q3 at 3,5
+after checking all col safe 
+place q4 at 4,2
+no safe place found so backtrack remove q4 and try new col for row 4
+move to q4 at 4,7
+place at q5 5,2
+place at q6 6,4
+place at q7 7,6
+no safe place found so backtrack again remove last few q
+final q8 at 8,4
