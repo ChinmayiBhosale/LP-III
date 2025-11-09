c73ecd53d8b351da1cad6b55b16cc38bd336f7f1
ChinmayiBhosale <chinmayibhosale28@gmail.com>
2025-11-09 21:38:36 +0530

    FractionalKnapsack.java

--- FractionalKnapsack.java
+++ FractionalKnapsack.java
@@ -59,3 +59,36 @@ public class FractionalKnapsack {
         System.out.println("Maximum value in the knapsack = " + maxValue);
     }
 }
+
+
+
+import java.util.*;
+
+class Item {
+    int weight, value;
+    Item(int w, int v) { weight = w; value = v; }
+}
+
+public class Fractional {
+    public static void main(String[] args) {
+        Item[] items = { new Item(10, 60), new Item(20, 100), new Item(30, 120) };
+        int capacity = 50;
+
+        Arrays.sort(items, (a, b) -> Double.compare((double)b.value/b.weight, (double)a.value/a.weight));
+
+        double totalValue = 0;
+        for (Item item : items) {
+            if (capacity >= item.weight) {
+                capacity -= item.weight;
+                totalValue += item.value;
+            } else {
+                totalValue += (double)item.value * capacity / item.weight;
+                break;
+            }
+        }
+        System.out.println("Maximum value = " + totalValue);
+    }
+}
+
+Time Complexity: O(n log n)
+Space Complexity: O(n)
